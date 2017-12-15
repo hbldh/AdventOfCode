297413dbcc2ef205ff7ab3459e41c200b43b61e1
Henrik Blidh <henrik.blidh@swedwise.com>
2017-12-15 08:32:07 +0100

    Completed Day 9

--- AOC2017/day09.py
+++ AOC2017/day09.py
@@ -2,20 +2,22 @@ def _remove_excl(data):
     out = []
     skip_one = False
     for c in data:
+        if skip_one:
+            skip_one = False
+            continue
         if c == '!':
             skip_one = True
-            continue
-        elif skip_one:
-            skip_one = False
             continue
         out.append(c)
     return "".join(out)
 
+
 def _remove_garbage(data):
+    count = 0
     out = []
     in_garbage = False
     for c in data:
-        if c == '<':
+        if c == '<' and not in_garbage:
             in_garbage = True
             continue
         if in_garbage and c == '>':
@@ -23,41 +25,48 @@ def _remove_garbage(data):
             continue
         if not in_garbage:
             out.append(c)
-    return "".join(out)
+        else:
+            count += 1
+    return "".join(out), count
 
 
 def _extract_root_groups(data):
+    score = 0
     out = []
-    group = []
+    groups = []
     group_depth = 0
     in_group = False
     for c in data:
-        if c == '{' and not in_group:
+        if c == '{':
             group_depth += 1
             in_group = True
+            score += group_depth
+            out.append(c)
         elif c == '}' and in_group:
             group_depth -= 1
-            if group_depth == 0:
-                out.append(c)
-                group.append("".join(out))
-        if in_garbage and c == '>':
-            in_garbage = False
-            continue
-        if not in_garbage:
             out.append(c)
-    return "".join(out)
+            if group_depth == 0:
 
+                groups.append(("".join(out), score))
+                out = []
+                score = 0
+        elif in_group:
+            out.append(c)
+
+    return groups
 
 
 def solve_1(indata):
     data = _remove_excl(indata)
-    data = _remove_garbage(data)
-    gropus = _extract_root_groups(data)
-    return 0
+    data, _ = _remove_garbage(data)
+    groups = _extract_root_groups(data)
+    return sum([g[1] for g in groups])
 
 
 def solve_2(indata):
-    return 0
+    data = _remove_excl(indata)
+    data, garbage_amount = _remove_garbage(data)
+    return garbage_amount
 
 
 def main():
@@ -66,8 +75,6 @@ def main():
     ensure_data(9)
     with open('input_09.txt', 'r') as f:
         data = f.read().strip()
-
-    data = """{{<a!>},{<a!>},{<a!>},{<ab>}}"""
 
     print("Part 1: {0}".format(solve_1(data)))
     print("Part 2: {0}".format(solve_2(data)))
