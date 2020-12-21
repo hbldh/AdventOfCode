93746813c8780e560ba3e63fd68ccf609f042cb9
Henrik Blidh <henrik.blidh@swedwise.com>
2020-12-21 21:12:48 +0100

    Black for Day 21

--- AOC2020/python/day21.py
+++ AOC2020/python/day21.py
@@ -1,20 +1,14 @@
 import itertools
 
-_data = """mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
-trh fvjkl sbzzf mxmxvkd (contains dairy)
-sqjhc fvjkl (contains soy)
-sqjhc mxmxvkd sbzzf (contains fish)"""
 
 def solve(data: str):
-    #data = _data
-
     allergen_possibilities = {}
     ingredient_rows = []
     for row in data.splitlines():
-        first, second = row.split(' (contains')
-        ingredients = first.split(' ')
+        first, second = row.split(" (contains")
+        ingredients = first.split(" ")
         ingredient_rows.append(ingredients)
-        allergens = tuple(map(lambda x: x.strip(), second.rstrip(')').split(',')))
+        allergens = tuple(map(lambda x: x.strip(), second.rstrip(")").split(",")))
         for allergen in allergens:
             if allergen in allergen_possibilities:
                 allergen_possibilities[allergen].intersection_update(ingredients)
@@ -29,16 +23,26 @@ def solve(data: str):
     changed = True
     while changed:
         changed = False
-        cannot_be = set([list(possibilities)[0] for allergen, possibilities in allergen_possibilities.items() if len(possibilities) == 1])
+        cannot_be = set(
+            [
+                list(possibilities)[0]
+                for allergen, possibilities in allergen_possibilities.items()
+                if len(possibilities) == 1
+            ]
+        )
         for allergen, possibilities in allergen_possibilities.items():
             if len(possibilities) != 1:
                 allergen_possibilities[allergen] = possibilities.difference(cannot_be)
                 changed = True
 
-    canonical_dangerous_ingredient_list = ",".join([allergen_possibilities[ingredient].pop() for ingredient in sorted(allergen_possibilities.keys())])
+    canonical_dangerous_ingredient_list = ",".join(
+        [
+            allergen_possibilities[ingredient].pop()
+            for ingredient in sorted(allergen_possibilities.keys())
+        ]
+    )
 
     return part_1, canonical_dangerous_ingredient_list
-
 
 
 if __name__ == "__main__":
